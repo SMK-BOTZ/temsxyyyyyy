867cda8ce149830c25f574655042caf5654e039f
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2024-11-25 19:40:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ else:
     URL = "https://megarenmaer-92447cf4dd07.herokuapp.com/".format(FQDN)
 
 # add premium logs channel id
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', ''))
+PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002056677294'))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
